8e15f08d9ae728b72597b2ecfe7623dfcacff177
IloveNooodles <mgarebaldhie80@gmail.com>
2023-04-22 21:01:01 +0700

    feat: adding simple endpoint for enqueue and dequeue

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,20 @@
-use axum::{routing::get, Router, Server};
-use raft_consensus::client;
-use raft_consensus::mq;
-use raft_consensus::network;
+use axum::{routing::get, routing::post, Router, Server};
+use log::Level;
+use raft_consensus::{client, logger, mq, network};
+use std::{net::SocketAddr, sync::Arc};
 
-use std::net::SocketAddr;
 /// Example tokio server
-///
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(route_get_handler));
+    // let mut state = mq::AppState::new();
+
+    let router = Router::new()
+        .route("/", get(route_get_handler))
+        .route("/enqueue", post(enqueue))
+        .route("/dequeue", post(dequeue));
 
     let server = Server::bind(&"0.0.0.0:8080".parse().unwrap()).serve(router.into_make_service());
+
     let sock_addr: SocketAddr = server.local_addr();
     println!("{:#?}", sock_addr);
 
@@ -24,4 +28,10 @@ async fn route_get_handler() -> &'static str {
     "Hi from axum"
 }
 
-struct AppState {}
+async fn enqueue() -> &'static str {
+    "Hi from axum"
+}
+
+async fn dequeue() -> &'static str {
+    "Hi from axum"
+}
